0f84a89f6150dc8c424231559a1253ea23498ed0
Anmol-dev45 <basnetanmol2020@gmail.com>
2024-09-02 14:12:14 +0545

    added clear btn

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,3 +1,4 @@
+import Clear from "./Clear/Clear";
 import Pen from "./Pen/Pen";
 import ColorPicker from "./ColorPicker/ColorPicker";
 import Eraser from "./Eraser/Eraser";
@@ -6,6 +7,7 @@ import Range from "./Range/Range";
 const Toolbar = () => {
   return (
     <div className="flex flex-col gap-4 items-center mt-7 ml-3">
+      <Clear />
       <ColorPicker />
       <Range />
       <Pen />
